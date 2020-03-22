4e3c80afc8209d58f621ecbc3f81c770659e0e50
Lucas Alves <lcluphs@gmail.com>
2020-03-22 11:51:45 -0300

    first commit

---
+++ src/test/java/com/lucas/examplemongo/ExampleMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.examplemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
