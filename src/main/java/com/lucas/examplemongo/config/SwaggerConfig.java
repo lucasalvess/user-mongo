3eba529bfc379d952a5d9df951243163531f58e4
Lucas Alves <lcluphs@gmail.com>
2020-03-23 09:44:22 -0300

    configure swagger

---
+++ src/main/java/com/lucas/examplemongo/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.lucas.examplemongo.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.lucas.examplemongo"))
+				.paths(PathSelectors.regex("/api.*")).build().apiInfo(metaInfo());
+	}
+
+	private ApiInfo metaInfo() {
+		 return new ApiInfo("Mongo users", "REST API with NoSql database", "1.0", "urn:tos",
+		          new Contact("Lucas Alves", "sa", "luca.lopes97@hotmail.com"), "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0",new ArrayList<VendorExtension>());
+
+	}
+
+}
