be782be54422526908bee4cbbc7829ffd0fa1a72
lucasalvess <lcluphus@gmail.com>
2020-08-16 22:18:13 -0300

    add redis depency + configure redis properties + configure docker + enable redis

--- src/main/java/com/lucas/examplemongo/ExampleMongoApplication.java
+++ src/main/java/com/lucas/examplemongo/ExampleMongoApplication.java
@@ -2,8 +2,10 @@ package com.lucas.examplemongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ExampleMongoApplication {
 
 	public static void main(String[] args) {
